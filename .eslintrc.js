d7876e2cf6aba46d17e39db663289f2e499baa2f
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-05-12 18:05:22 +0200

    add swim calculator logic

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'require-jsdoc': ['error', {
+      'require': {
+        'FunctionDeclaration': false,
+        'MethodDefinition': false,
+        'ClassDeclaration': false,
+        'ArrowFunctionExpression': false,
+        'FunctionExpression': false,
+      },
+    }],
+  },
+};
