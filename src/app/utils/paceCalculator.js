d11563ba0832f51bb5fc2765ab16d9aec3de8e6d
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-06-04 18:52:27 +0200

    add test fore paceCalculator.js => paceCalculator.test.js

--- src/app/utils/paceCalculator.js
+++ src/app/utils/paceCalculator.js
@@ -1,15 +1,19 @@
-export function paceCalculator(distance, time) {
-  if (distance===0 || !distance) {
-    return [0, 0, 0];
-  }
+export function paceCalculator(distance=1, time=[0, 0, 0]) {
+  const timeHour = parseInt(time[0]);
+  const timeMinute = parseInt(time[1]);
+  const timeSecond = parseInt(time[2]);
 
-  const totalSeconds = parseInt(time[0]) * 3600 + parseInt(time[1]) * 60 + parseInt(time[2]);
-  const paceInSeconds = (totalSeconds / distance).toFixed(2);
+  const totalTimeInSeconds = timeHour * 3600 + timeMinute * 60 + timeSecond;
+
+
+  const paceInSeconds = (totalTimeInSeconds / distance).toFixed(2);
   const paceInMinutes = Math.floor(paceInSeconds / 60).toString();
   const paceInSecondsMSeconds = (paceInSeconds - paceInMinutes * 60).toFixed(1);
   const paceSeconds = (paceInSecondsMSeconds.split('.')[0]);
   const paceMSeconds = (paceInSecondsMSeconds.split('.')[1]);
   return [paceInMinutes, paceSeconds, paceMSeconds];
 }
+
+console.log(paceCalculator(undefined, [0, 0, 0]));
 
 
--- src/app/utils/paceCalculator.test.js
+++ src/app/utils/paceCalculator.test.js
@@ -11,33 +11,23 @@ describe('paceCalculator', () => {
     expect(result).toEqual(expectedPace);
   });
 
-  it('should handle zero distance', () => {
-    const distance = 0;
+  it('should omit first argument', () => {
+    const distance = undefined;
     const time = ['1', '0', '0'];
-    const expectedPace = [0, 0, 0];
+    const expectedPace = ['60', '0', '0'];
 
     const result = paceCalculator(distance, time);
 
     expect(result).toEqual(expectedPace);
   });
 
-  it('should handle zero time', () => {
-    const distance = 5;
-    const time = ['0', '0', '0'];
+  it('should omit all arguments', () => {
+    const distance = undefined;
+    const time = undefined;
     const expectedPace = ['0', '0', '0'];
 
     const result = paceCalculator(distance, time);
 
     expect(result).toEqual(expectedPace);
   });
-  it('should is no distance value', () => {
-    const time = ['1', '10', '30'];
-    const expectedPace = ['0', '0', '0'];
-
-    const result = paceCalculator(time);
-
-    expect(result).toEqual(expectedPace);
-  });
-
-  // Add more test cases as needed
 });
